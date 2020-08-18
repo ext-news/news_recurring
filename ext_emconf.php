148b9c893ac3476df98ff77dc03a44cd839f11b9
Georg Ringer <georg.ringer@gmail.com>
2020-08-18 20:59:06 +0200

    [TASK] Fix requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-10.4.99',
-            'news' => '8.0.0-8.99.99',
+            'news' => '8.4.0-8.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
