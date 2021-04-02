1d3cf161dbcac5f329dde9f7dcac6dcca2815e0e
Georg Ringer <georg.ringer@gmail.com>
2021-04-02 21:16:19 +0200

    [TASK] Allow TYPO3 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mail@ringer.it',
     'state' => 'alpha',
     'clearCacheOnLoad' => true,
-    'version' => '3.0.0',
+    'version' => '3.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
-            'news' => '8.4.0-8.99.99',
+            'typo3' => '10.4.0-11.1.99',
+            'news' => '8.4.0-9.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
